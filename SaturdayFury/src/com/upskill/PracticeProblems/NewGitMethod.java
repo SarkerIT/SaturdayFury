76cd8dd1f5be1eb5262895c1bc5daeab2f6dbf6b
SarkerIT <sarker.data@gmail.com>
2024-12-08 00:52:37 -0600

    a comment

--- SaturdayFury/src/com/upskill/PracticeProblems/NewGitMethod.java
+++ SaturdayFury/src/com/upskill/PracticeProblems/NewGitMethod.java
@@ -5,6 +5,7 @@ public class NewGitMethod {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Just a bare comment!");
+		System.out.println("Just a bare comment! pushing from another branch");
 	}
 
 }
