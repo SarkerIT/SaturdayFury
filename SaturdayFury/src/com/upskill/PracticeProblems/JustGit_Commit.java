4c0fe56f1cf6312b9153817e9a494c0fc9d32525
SarkerIT <sarker.data@gmail.com>
2024-12-08 00:18:06 -0600

    commit from hotfix2

---
+++ SaturdayFury/src/com/upskill/PracticeProblems/JustGit_Commit.java
@@ -0,0 +1,14 @@
+package com.upskill.PracticeProblems;
+
+public class JustGit_Commit {
+	
+	public static void gitDemo() {
+		System.out.println("Just a commit of t Git");
+	}
+	
+	public static void main(String[] args) {
+		
+		gitDemo();
+	}
+
+}
