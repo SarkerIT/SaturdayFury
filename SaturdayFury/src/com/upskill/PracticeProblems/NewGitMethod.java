79c73ee6e00699adac498f45092650bcd98a18d0
SarkerIT <sarker.data@gmail.com>
2024-12-08 00:58:59 -0600

    new commit

---
+++ SaturdayFury/src/com/upskill/PracticeProblems/NewGitMethod.java
@@ -0,0 +1,12 @@
+package com.upskill.PracticeProblems;
+
+public class NewGitMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just a bare comment!");
+		System.out.println("Just a bare comment! pushing from another branch");
+		System.out.println("Just a bare comment!");
+	}
+
+}
