fd3e427901280d72581accc3c114d06f8b39828e
SarkerIT <sarker.data@gmail.com>
2024-12-11 22:14:19 -0600

    date and time

---
+++ src/com/upskill/java_1/JavaDate.java
@@ -0,0 +1,24 @@
+package com.upskill.java_1;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class JavaDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate currentDateObj = LocalDate.now(); 	// create a date object
+		System.out.println("Todays date: " + currentDateObj);       		// print the current date now
+		
+		
+		LocalTime currentLocalTime = LocalTime.now();
+		System.out.println("Time now: "+ currentLocalTime);
+		
+		// Custom format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println("Time now: "+ currentLocalTime.format(formatter));
+
+	}
+
+}
