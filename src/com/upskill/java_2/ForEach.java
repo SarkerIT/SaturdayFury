98d5a751f66231bcb2e56bf01c97f191afe43f19
SarkerIT <sarker.data@gmail.com>
2024-12-12 21:54:28 -0600

    for-each loop comment added

--- src/com/upskill/java_2/ForEach.java
+++ src/com/upskill/java_2/ForEach.java
@@ -1,6 +1,11 @@
 package com.upskill.java_2;
 
 // The for-each loop is AKA enhanced for-each for loop java
+
+// In regular for loop, you manually control the index i
+
+// In the enhanced for loop, variable directly refers to each element in the array
+
 // for-each iterate over without the elements without explicitly using an index
 // syntax:
 /* 
@@ -18,7 +23,7 @@ public class ForEach {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// normal for loop
+		// normal for loop							// regular for loop, you manually control the index i
 		for(int i=0; i<num.length; i++) {
 			System.out.print(num[i] + " "); 
 		}
@@ -26,8 +31,8 @@ public class ForEach {
 		System.out.println();
 		System.out.println("-----------------");
 		
-		// enhanced for-each loop
-		for(int numbers : num) {				// read like - for numbers in (meaning :) num
+		// enhanced for-each loop					//	the enhanced for loop, variable directly refers to each element in the array
+		for(int numbers : num) {	// read like - for numbers in (meaning :) num
 			System.out.print(numbers + " ");
 		}
 
