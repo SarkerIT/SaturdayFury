6ed9242afed54dcf5febcd056a063cd04929d00a
SarkerIT <sarker.data@gmail.com>
2024-12-15 14:06:48 -0600

    Array comparison solved

--- src/com/upskill/PracticeProblems/CompareTwoArrays.java
+++ src/com/upskill/PracticeProblems/CompareTwoArrays.java
@@ -5,33 +5,33 @@ import java.util.ArrayList;
 // Problem: Compare two arrays and make a new array with the matching numbers
 
 public class CompareTwoArrays {
-	
-	static int[] arr1 = {1, 2, 4, 51, 40, 3, 10};
-	static int[] arr2 = {3, 1, 2, 41, 11, 13, 40};
+
+	static int[] arr1 = { 1, 2, 4, 51, 40, 3, 40 };
+	static int[] arr2 = { 3, 1, 2, 51, 11, 13, 40 };
 
 	static ArrayList<Integer> arr3 = new ArrayList<Integer>();
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for(int i=0; i<arr1.length; i++) {
-			if(arr1[i]==arr2[i]) {
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] == arr2[i]) {
 				arr3.add(arr1[i]);
 			}
 		}
-		
-		// Printing
-//		for(int i=0; i<arr3.length(); i++) {
-//			System.out.println(arr3[i]);
-//		}
 
+		Object combinedArray[] = arr3.toArray();
 
-		for(items: arr3) {
-			System.out.println(items);
+		// printing with enhanced for loop
+		for(Object item : combinedArray) {
+			System.out.println(item);
 		}
-		
-		
-		
+/*		
+		// Printing with for loop
+		for(int i=0; i<combinedArray.length; i++) {
+			System.out.println(combinedArray[i]);
+		}
+*/
 	}
-	
+
 }
